fa731bcd344e939c43895261584d071430c36b69
Luis Ortega <phoenixsoftwaredesign@gmail.com>
2023-06-30 14:09:04 -0500

    configuring database

---
+++ app/app/tests.py
@@ -0,0 +1,13 @@
+"""
+Sample test
+"""
+
+from app import calc
+from django.test import SimpleTestCase
+
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
